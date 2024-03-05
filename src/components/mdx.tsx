3014a25a908c3a3491ca4d83611f95c01a44d3c3
Christian Velez <christian.gerardo_@outlook.es>
2024-03-04 20:06:41 -0600

    article subtitle design

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -189,6 +189,37 @@ function CodeBlock({
    )
 }
 
+// Article subtitle
+// function Title({ children, href }: { children: string; href: string }) {
+//    return (
+//       <h1 className='font-semibold text-xl mb-4'>
+//          <a href={href} className='hover:text-blue-400 group'>
+//             {children}
+//             <span className='inline-flex opacity-0 ml-2 group-hover:opacity-100'>
+//                <svg viewBox='0 0 16 16' height='0.7em' width='0.7em'>
+//                   <g strokeWidth='1.2' fill='none' stroke='currentColor'>
+//                      <path
+//                         fill='none'
+//                         strokeLinecap='round'
+//                         strokeLinejoin='round'
+//                         strokeMiterlimit='10'
+//                         d='M8.995,7.005 L8.995,7.005c1.374,1.374,1.374,3.601,0,4.975l-1.99,1.99c-1.374,1.374-3.601,1.374-4.975,0l0,0c-1.374-1.374-1.374-3.601,0-4.975 l1.748-1.698'
+//                      ></path>
+//                      <path
+//                         fill='none'
+//                         strokeLinecap='round'
+//                         strokeLinejoin='round'
+//                         strokeMiterlimit='10'
+//                         d='M7.005,8.995 L7.005,8.995c-1.374-1.374-1.374-3.601,0-4.975l1.99-1.99c1.374-1.374,3.601-1.374,4.975,0l0,0c1.374,1.374,1.374,3.601,0,4.975 l-1.748,1.698'
+//                      ></path>
+//                   </g>
+//                </svg>
+//             </span>
+//          </a>
+//       </h1>
+//    )
+// }
+
 function Code({
    className,
    ...props
