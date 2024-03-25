00420556489c701dc7ccee709c2faf7478d08644
Christian Velez <christian.gerardo_@outlook.es>
2024-03-25 16:09:53 -0600

    hook useIsMounted

---
+++ src/hooks/useIsMounted.ts
@@ -0,0 +1,15 @@
+import { useEffect, useRef } from 'react'
+
+export function useIsMounted() {
+   const mountedRef = useRef(false)
+
+   useEffect(() => {
+      mountedRef.current = true
+
+      return () => {
+         mountedRef.current = false
+      }
+   }, [])
+
+   return mountedRef
+}
