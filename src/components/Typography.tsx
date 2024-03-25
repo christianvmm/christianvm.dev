e9248e0001da6f468922b13314acd2be44c2a3b7
Christian Velez <christian.gerardo_@outlook.es>
2024-03-24 23:56:28 -0600

    Typography component

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -1,10 +1,36 @@
 import { cn } from '@/utils/cn'
 
-export const textOff = 'text-zinc-600 dark:text-zinc-400'
+export const textOff = ''
 
-export function Typography({
+type Variants = {
+   pageTitle: string
+   title: string
+   subtitle: string
+   body: string
+   small: string
+}
+const variants: Variants = {
+   pageTitle: 'text-2xl font-semibold',
+   title: 'text-xl font-semibold',
+   subtitle: 'font-semibold',
+   body: 'text-zinc-600 dark:text-zinc-400 leading-7',
+   small: 'text-sm ',
+}
+
+const defaultElement = 'p'
+
+type TypographyProps<T extends React.ElementType = typeof defaultElement> =
+   PolymorphicProps<T> & {
+      variant?: keyof Variants
+   }
+
+export function Typography<T extends React.ElementType = 'p'>({
    className,
+   variant = 'body',
+   as,
    ...props
-}: React.ComponentPropsWithoutRef<'p'>) {
-   return <p className={cn(textOff, 'leading-7', className)} {...props} />
+}: TypographyProps<T>) {
+   const Component = as || 'p'
+
+   return <Component className={cn(variants[variant], className)} {...props} />
 }
