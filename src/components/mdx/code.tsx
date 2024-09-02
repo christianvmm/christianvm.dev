1ed6ae1ca897ea83171ca5d30260e75d9072a3a3
Christian Velez <christian.gerardo_@outlook.es>
2024-09-02 15:34:44 -0600

    refact: move code highlight to server-side

--- src/components/mdx/code.tsx
+++ src/components/mdx/code.tsx
@@ -1,50 +1,36 @@
-// 'use client'
-// import { cn } from '@/utils/cn'
-// import hljs from 'highlight.js'
-// import 'highlight.js/styles/nord.css'
-
-// export function Code({
-//    className,
-//    children,
-//    ...props
-// }: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-//    children: string
-// }) {
-//    if (typeof children !== 'string') return null
-
-//    const highlightedHtml = hljs.highlightAuto(children).value
-
-//    return (
-//       <code
-//          className={cn(className, 'overflow-x-auto p-3 pt-0  rounded-md')}
-//          dangerouslySetInnerHTML={{ __html: highlightedHtml }}
-//          data-highlighted='yes'
-//          {...props}
-//       />
-//    )
-// }
-
-'use client'
 import { cn } from '@/utils/cn'
 import hljs from 'highlight.js'
-import { useEffect } from 'react'
 import 'highlight.js/styles/nord.css'
 
-// TODO: fix layout flash
 export function Code({
    className,
-   ...props
+   children,
 }: React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
    children: string
 }) {
-   useEffect(() => {
-      hljs.highlightAll()
-   }, [])
+   if (typeof children !== 'string') return null
+
+   const languageStr = className?.split('-').at(-1)
+   if (!languageStr) return null
+
+   const language = hljs.getLanguage(languageStr)
+
+   if (!language) return null
+
+   const highlightedHtml = hljs.highlight(children, {
+      language: languageStr,
+   }).value
 
    return (
       <code
-         className={cn(className, 'overflow-x-auto p-3 pt-0  rounded-md')}
-         {...props}
+         className={cn(
+            'overflow-x-auto p-3 pt-0  rounded-md hljs',
+            className,
+            language.className
+         )}
+         dangerouslySetInnerHTML={{
+            __html: highlightedHtml,
+         }}
       />
    )
 }
